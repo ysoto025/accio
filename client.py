92b79ab4c9c36868b4e4f55e36f1b6b704588cbc
ysoto025 <55642058+ysoto025@users.noreply.github.com>
2020-09-13 20:47:19 -0400

    Update client.py

--- client.py
+++ client.py
@@ -7,6 +7,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2048)
 
+if len(argv) < 4:
+    print("missing arguments")
+    sys.exit()
+
 try:
     socket.gethostbyname(argv[1])
     print("Good Host")
@@ -15,10 +19,18 @@ except:
 
 try:
     sock.connect((argv[1], int(argv[2])))
+    print("It connected")
 except socket.error as err:
-    print("Could not connect to hostdue error:" + err)
+    print("Could not connect to host due error:" + err)
 
-send = sock.send(b"accio\r\n")
+file = open(argv[3], "rb")
+accio = (sock.recv(1024).decode("utf-8"))
+print(accio)
+# if accio == 'accio':
+print("File Wnt Thru")
+send = file.read(1024)
+sock.send(send)
 
-b = sock.recv(1024)
+file.close()
+
 sock.close()
