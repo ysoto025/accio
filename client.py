47d716468dd520a352e5362f3f8f84f03419114b
ysoto025 <55642058+ysoto025@users.noreply.github.com>
2020-09-20 13:47:18 -0400

    Update client.py

--- client.py
+++ client.py
@@ -7,29 +7,32 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2048)
 
-if len(argv) < 4:
+if len(argv) < 3:
     print("missing arguments")
     sys.exit()
-
-try:
-    socket.gethostbyname(argv[1])
-    print("Good Host")
-except:
-    print("Could not connect to host")
-
 try:
     sock.connect((argv[1], int(argv[2])))
     print("It connected")
-except socket.error as err:
+except sock.error as err:
     print("Could not connect to host due error:" + err)
+expect
 
 file = open(argv[3], "rb")
-accio = (sock.recv(1024).decode("utf-8"))
-print(accio)
-# if accio == 'accio':
-print("File Wnt Thru")
-send = file.read(1024)
-sock.send(send)
+accio = ""
+
+while True:
+    rec = (self.sock.recv(1).decode("utf-8"))
+    if len(rec) < 1:
+        break
+    accio += rec
+
+if accio == 'accio':
+    while True:
+
+        send = file.read(1024)
+        if len(send) < 1:
+            break
+        sock.send(send)
 
 file.close()
 
